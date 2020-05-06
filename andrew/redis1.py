4fd45f5497a5957f4f572a4698da319ba879b322
robinwu <robwu2@cisco.com>
2020-05-06 15:25:03 +0800

    add Redis

---
+++ andrew/redis1.py
@@ -0,0 +1,41 @@
+import redis as r
+import pickle
+import time
+
+POOL = r.ConnectionPool(host='localhost', port=6379, db=0)
+
+
+class Redis(object):
+    def __init__(self, name: str):
+        self.name = name
+        self.db = r.StrictRedis(connection_pool=POOL)
+        return
+
+    def push(self, value: str):
+        self.db.rpush(self.name, value)
+        return
+
+    def pop(self):
+        value = self.db.lpop(self.name)
+        return value.decode('utf-8') if value else ""
+
+    def append(self, key: str, value: str):
+        self.db.append(key="-".join([self.name, key]), value=value)
+        return
+
+    def set(self, key: str, value, expire=None):
+        while not self.db.setnx(self.name + '______redis', 'LOCK'):
+            time.sleep(0.1)
+        self.db.set(name="-".join([self.name, key]), value=pickle.dumps(value), ex=expire)
+        del self.db[self.name + '______redis']
+
+    def get(self, key: str):
+        value = self.db.get(name="-".join([self.name, key]))
+        return pickle.loads(value) if value else ""
+
+    def delete(self, key: str):
+        del self.db["-".join([self.name, key])]
+        return
+
+    def clean_up(self):
+        return self.db.flushall()
