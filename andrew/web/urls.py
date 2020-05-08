4737f5afd3aaacf1a3e6d6f0cdc71e4474e6feec
robinwu <robwu2@cisco.com>
2020-05-08 12:01:06 +0800

    add django

--- andrew/web/urls.py
+++ andrew/web/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^api/', include('gui.urls', namespace='gui')),
+    # url(r'^api/', include('gui.urls', namespace='gui')),
+    url(r'^admin/', admin.site.urls),
+
 ]
